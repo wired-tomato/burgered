d81a913ab7047741f8e1f7dc953532a94f98755b
wired-tomato <brokenfuse.self@gmail.com>
2024-07-30 23:43:18 -0400

    eat event

--- src/main/kotlin/net/wiredtomato/burgered/Burgered.kt
+++ src/main/kotlin/net/wiredtomato/burgered/Burgered.kt
@@ -2,6 +2,7 @@ package net.wiredtomato.burgered
 
 import net.minecraft.util.Identifier
 import net.wiredtomato.burgered.api.event.LivingEntityEvents
+import net.wiredtomato.burgered.api.ingredient.BurgerIngredient
 import net.wiredtomato.burgered.init.*
 import org.slf4j.LoggerFactory
 
@@ -17,7 +18,12 @@ object Burgered {
         BurgeredTabs
 
         LivingEntityEvents.ON_EAT.register onEat@ { entity, world, stack, component ->
-            val burger = stack.get(BurgeredDataComponents.BURGER) ?: return@onEat
+            val burger = stack.get(BurgeredDataComponents.BURGER) ?: run {
+                val item = stack.item
+                if (item is BurgerIngredient) item.onEat(entity, world, stack, component)
+
+                return@onEat
+            }
             burger.onEat(entity, world, stack, component)
         }
     }
